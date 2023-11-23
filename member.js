53951e2a84a03fc5dca681889f64dd92f416f9af
FeiNiao <84019101+1523369326@users.noreply.github.com>
2023-11-23 06:20:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember()
+{
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'skillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+
+    };
+}
+
